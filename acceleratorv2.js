c453b14371c473e7d70bb85a7e47ffa9d698eb73
Eliyahu Levy <Eliyahu.Levy21@gmail.com>
2021-02-04 15:37:47 -0500

    Update acceleratorv2.js

--- acceleratorv2.js
+++ acceleratorv2.js
@@ -1,6 +1,27 @@
 let attractors = [];
 let movers = [];
 
+/*
+A few cool arrangement of attractors
+0: Vector {p5: p5, x: 1125.23539134661, y: 100, z: 0}
+1: Vector {p5: p5, x: 1006.0685785647863, y: 254, z: 0}
+2: Vector {p5: p5, x: 977.1901996298396, y: 408, z: 0}
+3: Vector {p5: p5, x: 956.9116570762187, y: 562, z: 0}
+
+
+0: Vector {p5: p5, x: 958.4259874542411, y: 100, z: 0}
+1: Vector {p5: p5, x: 1152.7220060164436, y: 254, z: 0}
+2: Vector {p5: p5, x: 939.8697897869996, y: 408, z: 0}
+3: Vector {p5: p5, x: 849.4983257900872, y: 562, z: 0}
+
+0: Vector {p5: p5, x: 910.1091337420329, y: 100, z: 0}
+1: Vector {p5: p5, x: 1053.1826741711818, y: 254, z: 0}
+2: Vector {p5: p5, x: 1150.1677986333032, y: 408, z: 0}
+3: Vector {p5: p5, x: 1225.887315371726, y: 562, z: 0}
+
+*/
+
+
 
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
@@ -11,9 +32,9 @@ function setup() {
   // for(let i = 0; i < 1; i++) {
   //   movers[i] = new Mover(random(width),random(height));
   // }
-  movers[0] = new Mover(5*width/7,height/2);
+  movers[0] = new Mover(4*width/7,height/2);
   for(let i = 0; i < 4; i++) {
-    attractors[i] = createVector(random(4*width/7,width-100),i * height/5 + 100);
+    attractors[i] = createVector(random(3.5*width/7,5 * width/7),i * height/6 + height/6);
     fill(255,100);
     // ellipse(attractors[i].x,attractors[i].y,20);
   }
